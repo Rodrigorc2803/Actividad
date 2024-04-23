78e37afb1ea116dc0eebea102b694b08321bd85d
Rodrigorc2803 <rodrigorc2803@gmail.com>
2024-04-22 20:09:19 -0500

    Clase ultima

---
+++ GeneradorPrimoYPerfecto .cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad1
+{
+    public class GeneradorPrimoYPerfecto : GeneradorDeNumero
+    {
+        private bool buscarPrimo;
+
+        public GeneradorPrimoYPerfecto(bool buscarPrimo)
+        {
+            this.buscarPrimo = buscarPrimo;
+        }
+
+        public override int ObtenerNumero(int n)
+        {
+            int contador = 0;
+            int num = 2;
+            while (contador < n)
+            {
+                if (buscarPrimo && EsPrimo(num) || !buscarPrimo && EsPerfecto(num))
+                {
+                    contador++;
+                }
+                num++;
+            }
+            return num - 1;
+        }
+    }
+}
